b9b267e3e1459d83172d7dfbb54098e1d3b49b03
surendras <surendra.singh@tonkabi.com>
2023-04-07 11:37:55 +0530

    latest updated work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <></>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
